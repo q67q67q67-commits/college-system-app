03128bf99610d722fb8ef7defcba7a9b01c174c9
vivobook 5825 <q67q67q67@gmail.com>
2026-01-31 03:18:40 +0500

    webPrototype: remove Flutter (nc_app), web-only version

--- nc_app/lib/utils/download_helper.dart
+++
@@ -1,2 +0,0 @@
-export 'download_stub.dart'
-    if (dart.library.io) 'download_io.dart';
--- nc_app/lib/utils/download_stub.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/foundation.dart' show kIsWeb;
-
-Future<void> saveAndOpenFile(List<int> bytes, String filename) async {
-  if (kIsWeb) return; // Web: use web implementation
-  throw UnsupportedError('Use download_io for non-web');
-}
